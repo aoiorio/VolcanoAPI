5f52f8d7de811e2ed9648089a9627b55f8208917
aoiorio <130330538+aoiorio@users.noreply.github.com>
2024-05-31 17:37:55 +0900

    🍕 add: user_router

--- volcano/api/v1/routes.py
+++ volcano/api/v1/routes.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter
 
 from .endpoints.todo import router as todo_router
 from .endpoints.auth import router as auth_router
+from .endpoints.user import router as user_router
 
 routes = APIRouter()
 
 routes.include_router(todo_router)
 routes.include_router(auth_router)
+routes.include_router(user_router)
 
 
