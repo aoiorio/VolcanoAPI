4b00cb95c95aced25ef261a7bf30dce6cc73f3c1
aoiorio <130330538+aoiorio@users.noreply.github.com>
2024-05-31 17:36:30 +0900

    💡 feat: get user info feature

---
+++ volcano/domain/repository/user/user_repository.py
@@ -0,0 +1,11 @@
+from abc import abstractmethod, ABCMeta
+from typing import Optional
+from ...entity.user import VolcanoUser
+from fastapi import Request
+
+class UserRepository(metaclass=ABCMeta):
+
+    @classmethod
+    @abstractmethod
+    def get_user_info(self, token: str) -> Optional[VolcanoUser]:
+        ...
---
+++ volcano/infrastructure/repository/user/user_repository_impl.py
@@ -0,0 +1,18 @@
+from ....domain.repository.user.user_repository import UserRepository
+from ....domain.repository.auth.auth_repository import AuthRepository
+from ....infrastructure.repository.auth.auth_repository_impl import AuthRepositoryImpl
+
+from ....domain.entity.user import VolcanoUser
+from fastapi import Request
+from typing import Optional
+from sqlalchemy.orm import Session
+
+class UserRepositoryImpl(UserRepository):
+
+    def __init__(self, db: Session):
+        self.auth_repository: AuthRepository = AuthRepositoryImpl(db=db)
+
+    def get_user_info(self, token: str) -> Optional[VolcanoUser]:
+        volcano_user = self.auth_repository.get_current_user(token)
+        return volcano_user
+
