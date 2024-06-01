148fddb18e4194cb7ecb64ceb2f22eb276baafd2
aoiorio <130330538+aoiorio@users.noreply.github.com>
2024-06-01 11:41:35 +0900

    🍕 add: time_cost to hash a password

--- volcano/infrastructure/repository/auth/auth_repository_impl.py
+++ volcano/infrastructure/repository/auth/auth_repository_impl.py
@@ -6,32 +6,27 @@ from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
 from fastapi.responses import Response
+
 # from http.client import HTTPResponse
 from volcano.domain.entity.user import VolcanoUser
 from ...postgresql.dto.volcano_user_dto import VolcanoUserDTO
 from ....domain.repository.auth.auth_repository import AuthRepository
 from ....core.config import SECRET_KEY, ALGORITHM
-from passlib.hash import scrypt, sha256_crypt, argon2
-
-# from passlib.hash import sha256_crypt
+from passlib.hash import argon2
 
 
+# bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 # create hash password
 def create_password_hash(user_password: str):
     print("create_password_hash!!")
-    # hashed_password_class = hashlib.sha256(user_password.encode('utf-8'))
-    # hashed_password = hashed_password_class.hexdigest()
-    # hashed_password = scrypt.hash(user_password)
-    # print(f"hashed_password by using scrypt: {hashed_password}")
-    # hashed_password = sha256_crypt.encrypt(user_password)
-    # print(f"hashed_password by using sha256: {hashed_password}")
-    hashed_password = argon2.hash(user_password)
-    print(f"hashed_password by using argon2: {hashed_password}")
+    hashed_password = argon2.using(
+        time_cost=2, memory_cost=2**16, parallelism=4, salt_size=128, digest_size=256
+    ).hash(user_password)
 
-    # return bcrypt_context.hash(user_password)
+    print(hashed_password.split('$'))
+
     return hashed_password
 
 
@@ -51,27 +46,27 @@ class AuthRepositoryImpl(AuthRepository):
 
     def find_by_id(self, user_id: str) -> Optional[VolcanoUser]:
         try:
-            volcano_user_dto = self.db.query(VolcanoUserDTO).filter_by(user_id=user_id).first()
+            volcano_user_dto = (
+                self.db.query(VolcanoUserDTO).filter_by(user_id=user_id).first()
+            )
         except:
             raise
-        return volcano_user_dto # this will cause an error volcano_user_dto.to_entity()
+        return volcano_user_dto  # this will cause an error volcano_user_dto.to_entity()
 
     def find_by_email(self, email: str) -> Optional[VolcanoUser]:
         # hashed_password = create_password_hash(user_password)
         try:
-            volcano_user_dto = self.db.query(VolcanoUserDTO).filter_by(email=email).first()
+            volcano_user_dto = (
+                self.db.query(VolcanoUserDTO).filter_by(email=email).first()
+            )
         except:
             raise
         return volcano_user_dto
 
     # verify passwords that users inputted and hashed password
     def verify_user_password(self, plain_password: str, hashed_password: str) -> bool:
-        print(f"verify_user_password: {hashed_password}")
-        # result = scrypt.verify(plain_password, hashed_password)
-        result = argon2.verify(plain_password, hashed_password)
-        print(result)
-        # return bcrypt_context.verify(plain_password, hashed_password)
-        return result
+        # result = argon2.verify(plain_password, hashed_password)
+        return argon2.verify(plain_password, hashed_password)
 
     def sign_up(self, volcano_user: VolcanoUser) -> Optional[VolcanoUser]:
         print(f"volcano_user from repository impl file is here {volcano_user}")
@@ -91,12 +86,15 @@ class AuthRepositoryImpl(AuthRepository):
             raise
 
     def sign_in(self, user_id: str, response: Response) -> Optional[str]:
-        # response = Response
-        # response = HTTPResponse("")
         # NOTE it's a different function from creating cookie , so you can do it in another function called sign_in_for_token() or you can rename this function to it.
         # NOTE Then it will return a string access token (JWT)
         try:
-            access_token = create_access_token(data={'sub': str(user_id),}, expires_delta=300)
+            access_token = create_access_token(
+                data={
+                    "sub": str(user_id),
+                },
+                expires_delta=300,
+            )
             print(f"Here is access_token {access_token}")
             response.set_cookie(key="access_token", value=access_token, httponly=True)
             return access_token
@@ -109,7 +107,9 @@ class AuthRepositoryImpl(AuthRepository):
             payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
             user_id: str = payload.get("sub")
             print(user_id)
-            volcano_user_dto = self.db.query(VolcanoUserDTO).filter_by(user_id=user_id).first()
+            volcano_user_dto = (
+                self.db.query(VolcanoUserDTO).filter_by(user_id=user_id).first()
+            )
 
             return volcano_user_dto
         except JWTError:
@@ -120,7 +120,7 @@ class AuthRepositoryImpl(AuthRepository):
     def sign_out(self, response: Response) -> Optional[VolcanoUser]:
         # response = Response
         try:
-            print("this is sign_out!!")
+            print("You will sign out!")
             response.delete_cookie(key="access_token")
         except:
             raise
